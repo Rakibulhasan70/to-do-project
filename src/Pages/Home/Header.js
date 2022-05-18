4722673cff96714fd2b18054c47fa2c04af7b807
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-05-18 21:40:42 +0600

    project setup and header done

---
+++ src/Pages/Home/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" style={{ background: 'darkcyan' }} variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">To-do project</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 style={{ color: 'purple' }} className='text-center mt-5'>My To-Do Project</h2>
+        </div>
+    );
+};
+
+export default Home;
